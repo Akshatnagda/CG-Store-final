539c7418e07db43a181c6786cc2e0e566fad953a
Akshatnagda <100136193+Akshatnagda@users.noreply.github.com>
2023-12-20 17:50:19 +0530

    Update Footer.js

--- Client/src/components/footer/Footer.js
+++ Client/src/components/footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
                 <div className="container px-15 py-24 mx-auto">
                     <div className="flex flex-wrap md:text-left text-center -mb-10 -mx-5">
                     
-                        <div className="lg:w-1/6 md:w-1/2 w-full px-6">
+                        <div className="lg:w-1/6 md:w-1/2 w-full px-4">
                             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">ABOUT US</h2>
                             <nav className="list-none mb-10">
                                 <li>
